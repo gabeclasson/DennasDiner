d9d336b76f7ba6150bc8e879c798f0c0f8d0e164
mr-yman <gabriel.classon@gmail.com>
2019-09-20 11:59:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dennasdiner/Food.java
@@ -0,0 +1,58 @@
+package com.example.dennasdiner;
+
+public class Food {
+    private String foodName, description;
+    private double price;
+    private int imageResourceID;
+
+    public static final Food[] breakfastFoods = {
+            new Food("Eggs", "Three unfertilized ova of one or more members of the species Gallus gallus domesticus, thoroughly mixed with your selection of fermented solid bovine milk solids and animal flesh, cooked until safe to eat", 8.99, R.drawable.omelet),
+            new Food ("Pancakes", "3 pankcakes, choice of meat, potato", 7.95, R.drawable.pancakes),
+            new Food("Waffles", "Belgian waffles, whipped cream, fresh fruit", 7.50, R.drawable.waffles)
+    };
+
+    public Food(String foodName, String description, double price, int imageResourceID){
+        this.foodName = foodName;
+        this.description = description;
+        this.price = price;
+        this.imageResourceID = imageResourceID;
+    }
+
+    @Override
+    public String toString(){
+        return foodName;
+    }
+
+
+    public String getFoodName() {
+        return foodName;
+    }
+
+    public void setFoodName(String foodName) {
+        this.foodName = foodName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getImageResourceID() {
+        return imageResourceID;
+    }
+
+    public void setImageResourceID(int imageResourceID) {
+        this.imageResourceID = imageResourceID;
+    }
+}
---
+++ app/src/main/java/com/example/dennasdiner/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.dennasdiner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        AdapterView.OnItemClickListener itemClickListener =
+                new AdapterView.OnItemClickListener(){
+                    public void onItemClick(AdapterView<?> listView, View itemView, int position, long id){
+                        if (position == 0){
+                            Intent intent = new Intent(MainActivity.this, BreakfastChoicesActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                };
+        // add the listener to list view
+        ListView listview = findViewById(R.id.mealChoices);
+        listview.setOnItemClickListener(itemClickListener);
+    }
+
+
+}
